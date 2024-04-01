6ff334aa77e40c4a2e5838f2a56e340d095d1ab8
Choo <choo000407@naver.com>
2024-04-02 04:18:30 +0900

    test: service 통합테스트 추가

---
+++ src/test/java/chess/service/BoardServiceIntegrationTest.java
@@ -0,0 +1,114 @@
+package chess.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import chess.dao.BoardDao;
+import chess.dao.PieceDao;
+import chess.database.DatabaseConnectionManager;
+import chess.database.TransactionManager;
+import chess.model.board.Board;
+import chess.model.piece.Color;
+import chess.model.piece.PieceFixture;
+import chess.model.position.Movement;
+import chess.model.position.Position;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BoardServiceIntegrationTest {
+    private static final String TEST_PROPERTIES_PATH = "src/main/java/chess/resource/test.yml";
+
+    BoardService boardService;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        DatabaseConnectionManager databaseConnectionManager = DatabaseConnectionManager.from(TEST_PROPERTIES_PATH);
+        Connection connection = databaseConnectionManager.getConnection();
+        initializeDatabase(connection);
+        BoardDao boardDao = new BoardDao(connection);
+        PieceDao pieceDao = new PieceDao(connection);
+        TransactionManager transactionManager = new TransactionManager(connection);
+        boardService = new BoardService(boardDao, pieceDao, transactionManager);
+    }
+
+    private void initializeDatabase(Connection connection) throws SQLException {
+        connection.prepareStatement("DELETE FROM piece").execute();
+        connection.prepareStatement("DELETE FROM board").execute();
+        connection.prepareStatement(
+                        "INSERT INTO board (id, current_color, winner_color) VALUES (1, 'WHITE', 'NONE')")
+                .execute();
+        connection.prepareStatement(
+                        "INSERT INTO piece (id, board_id, type, color, file, `rank`) VALUES (1, 1, 'KING', 'WHITE', 1, 1)")
+                .execute();
+        connection.prepareStatement(
+                        "INSERT INTO piece (id, board_id, type, color, file, `rank`) VALUES (2, 1, 'KING', 'BLACK', 1, 2)")
+                .execute();
+    }
+
+    @Test
+    void 마지막_보드를_조회해_우승자가_결정되지_않았다면_기존_보드를_사용한다() {
+        // when
+        Board board = boardService.getRunningBoard();
+
+        // then
+        List<String> boardLines = PieceFixture.mappingBoard(board);
+        assertThat(boardLines).containsExactly(
+                "........",
+                "........",
+                "........",
+                "........",
+                "........",
+                "........",
+                "K.......",
+                "k......."
+        );
+    }
+
+    @Test
+    void 마지막_보드를_조회해_우승자가_결정되었다면_새_보드를_생성한다() {
+        // given
+        boardService.updateWinner(Color.WHITE);
+
+        // when
+        Board board = boardService.getRunningBoard();
+
+        // then
+        List<String> boardLines = PieceFixture.mappingBoard(board);
+        assertThat(boardLines).containsExactly(
+                "RNBQKBNR",
+                "PPPPPPPP",
+                "........",
+                "........",
+                "........",
+                "........",
+                "pppppppp",
+                "rnbqkbnr"
+        );
+    }
+
+    @Test
+    void 기물이_움직일_수_있다면_기물의_위치와_턴을_업데이트한다() {
+        // given
+        Board board = boardService.getRunningBoard();
+        Movement movement = new Movement(Position.of(1, 2), Position.of(1, 1));
+
+        // when
+        boardService.updatePieceAndTurn(board, movement);
+
+        // then
+        Board updatedBoard = boardService.getRunningBoard();
+        List<String> boardLines = PieceFixture.mappingBoard(updatedBoard);
+        assertThat(boardLines).containsExactly(
+                "........",
+                "........",
+                "........",
+                "........",
+                "........",
+                "........",
+                "........",
+                "K......."
+        );
+    }
+}
